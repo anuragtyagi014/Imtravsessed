90a7ce32a1a0ccee79c49da189f4c12d7e2ababf
Anurag <anuragtyagi014@gmail.com>
2024-05-09 10:30:47 +0530

    Fixed bug

--- comments.php
+++ comments.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying comments
  *
@@ -12,32 +13,34 @@
 
 namespace Kadence;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
+
+
 
 /*
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
+if (post_password_required()) {
 	return;
 }
-kadence()->print_styles( 'kadence-comments' );
+kadence()->print_styles('kadence-comments');
 ?>
-<div id="comments" class="comments-area<?php echo ( kadence()->option( 'post_footer_area_boxed' ) ? ' content-bg entry-content-wrap entry' : '' ); ?>">
+<div id="comments" class="comments-area<?php echo (kadence()->option('post_footer_area_boxed') ? ' content-bg entry-content-wrap entry' : ''); ?>">
 	<?php
-	do_action( 'kadence_before_comments' );
+	do_action('kadence_before_comments');
 	/**
 	 * Kadence Comments hook.
 	 *
 	 * @hooked Kadence/comments_list - 10
 	 * @hooked Kadence/comments_form - 15/5
 	 */
-	do_action( 'kadence_comments' );
+	do_action('kadence_comments');
 
-	do_action( 'kadence_after_comments' );
+	do_action('kadence_after_comments');
 
 	?>
 </div><!-- #comments -->
